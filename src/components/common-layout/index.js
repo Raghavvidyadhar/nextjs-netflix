4c4be8453c66270b0e21350e23af5d2f6117e6f6
Raghav vidyadhar <raghavvidyadhar55@gmail.com>
2024-09-06 17:37:41 +0530

    first commit

--- src/components/common-layout/index.js
+++ src/components/common-layout/index.js
@@ -1,44 +1,39 @@
-'use client'
+"use client";
 
-import Navbar from "../navbar"
-import { motion } from "framer-motion"
-import Head from "next/head"
-import MediaRow from "../media-row"
-import Banner from "../banner"
+import { motion } from "framer-motion";
+import Head from "next/head";
+import Navbar from "../navbar";
+import MediaRow from "../media-row";
+import Banner from "../banner";
+
 export default function CommonLayout({ mediaData }) {
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-        >
-            <Head>
-                <title>Netflix</title>
-                {/* { to do -> to add all other properties } */}
-            </Head>
-            <>
-                <Navbar />
-                <div className="relative pl-4 pb-24 lg:space-y-24 ">
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+    >
+      <Head>
+        <title>Netflix Clone</title>
+        {/* to do -> to add all other properties */}
+      </Head>
+      <>
+        <Navbar />
+        <div className="relative pl-4 pb-24 lg:space-y-24">
+          <Banner
+            medias={mediaData && mediaData.length ? mediaData[0].medias : []}
+          />
+         <section className="md:space-y-16">
+  {mediaData && mediaData.length
+    ? mediaData.map((item, index) => (
+        <MediaRow key={item.id || index} title={item.title} medias={item.medias} />
+      ))
+    : null}
+</section>
 
-             <Banner medias={mediaData && mediaData.length ? mediaData[0].medias : []}
-             
-             
-             />
-
-              
-                <section className="md:space-y-16">
-                   {mediaData && mediaData.length 
-                    ? mediaData.map((item)=>(
-                       <MediaRow title={item.title}  medias={item.medias}/>
-
-                    ))
-                   
-                   :null } 
-
-                </section>
+         
                 </div>
-
-            </>
-        </motion.div>
-    );
+      </>
+    </motion.div>
+  );
 }
